371d69eb1460c6f5c43b00aef6ecdf56d77071c8
Alberto <aldo10012@gmail.com>
2021-08-25 16:42:52 -0700

    created Post model

---
+++ ProductHunt/ProductHunt/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  ProductHunt
+//
+//  Created by Alberto Dominguez on 8/25/21.
+//
+
+import Foundation
+
+struct Post {
+   let id: Int
+   let name: String
+   let tagline: String
+   let votesCount: Int
+   let commentsCount: Int
+}
